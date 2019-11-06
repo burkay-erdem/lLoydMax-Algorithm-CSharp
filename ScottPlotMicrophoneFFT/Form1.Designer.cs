832686defd4b6a9a973a9f727f7befa641b3b8ec
erdem3801 <burkaerdem@gmail.com>
2019-11-06 21:58:15 +0300

    selam olsun güzel insanlara

---
+++ ScottPlotMicrophoneFFT/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ScottPlotMicrophoneFFT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer double[]iable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerReplot = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.scottPlotUC2 = new ScottPlot.ScottPlotUC();
+            this.scottPlotUC1 = new ScottPlot.ScottPlotUC();
+            this.scottPlotUC3 = new ScottPlot.ScottPlotUC();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timerReplot
+            // 
+            this.timerReplot.Interval = 2;
+            this.timerReplot.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 822);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1350, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(154, 17);
+            this.lblStatus.Text = "toolStripStatusLabel1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "cal";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // scottPlotUC2
+            // 
+            this.scottPlotUC2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.scottPlotUC2.Location = new System.Drawing.Point(0, 274);
+            this.scottPlotUC2.Margin = new System.Windows.Forms.Padding(2);
+            this.scottPlotUC2.Name = "scottPlotUC2";
+            this.scottPlotUC2.Padding = new System.Windows.Forms.Padding(0, 0, 0, 20);
+            this.scottPlotUC2.Size = new System.Drawing.Size(1350, 274);
+            this.scottPlotUC2.TabIndex = 1;
+            // 
+            // scottPlotUC1
+            // 
+            this.scottPlotUC1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.scottPlotUC1.Location = new System.Drawing.Point(0, 0);
+            this.scottPlotUC1.Margin = new System.Windows.Forms.Padding(2);
+            this.scottPlotUC1.Name = "scottPlotUC1";
+            this.scottPlotUC1.Padding = new System.Windows.Forms.Padding(0, 20, 0, 0);
+            this.scottPlotUC1.Size = new System.Drawing.Size(1350, 274);
+            this.scottPlotUC1.TabIndex = 0;
+            // 
+            // scottPlotUC3
+            // 
+            this.scottPlotUC3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.scottPlotUC3.Location = new System.Drawing.Point(0, 548);
+            this.scottPlotUC3.Margin = new System.Windows.Forms.Padding(2);
+            this.scottPlotUC3.Name = "scottPlotUC3";
+            this.scottPlotUC3.Size = new System.Drawing.Size(1350, 274);
+            this.scottPlotUC3.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(133, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 18);
+            this.label1.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1217, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Auto scale";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1367, 729);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.scottPlotUC3);
+            this.Controls.Add(this.scottPlotUC2);
+            this.Controls.Add(this.scottPlotUC1);
+            this.Name = "Form1";
+            this.Text = "ScottPlot Microphone FFT Demo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timerReplot;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.Button button1;
+        private ScottPlot.ScottPlotUC scottPlotUC2;
+        private ScottPlot.ScottPlotUC scottPlotUC1;
+        private ScottPlot.ScottPlotUC scottPlotUC3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ScottPlotMicrophoneFFT/Form1.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ScottPlotMicrophoneFFT
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupGraphLabels();
+            // timerReplot.Enabled = true;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        public void SetupGraphLabels()
+        {
+            scottPlotUC1.fig.labelTitle = "INPUTS";
+            scottPlotUC1.fig.labelY = "Inputs ";
+            scottPlotUC1.fig.labelX = "index";
+            scottPlotUC1.Redraw();
+
+            scottPlotUC2.fig.labelTitle = "GAUSS Distribution";
+            scottPlotUC2.fig.labelY = "Gauss POW";
+            scottPlotUC2.fig.labelX = "Index";
+            scottPlotUC2.Redraw();
+
+            scottPlotUC3.fig.labelTitle = "Mean Square Error";
+            scottPlotUC3.fig.labelY = "MSE";
+            scottPlotUC3.fig.labelX = "Index";
+            scottPlotUC3.Redraw();
+        }
+
+
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // turn off the timer, take as long as we need to plot, then turn the timer back on
+            timerReplot.Enabled = false;
+            PlotLatestData();
+            timerReplot.Enabled = true;
+        }
+
+        public int numberOfDraws = 0;
+        public bool needsAutoScaling = true;
+        public void PlotLatestData()
+        {
+
+
+
+
+            int mseindex = 0;
+            int M = 20;
+            double[] mse_arr = new double[M];
+            double[] aJ = new double[M + 1];
+            double[] bJ = new double[M];
+            double[] gauss = new double[M + 1];
+            double MSE_er=0;
+
+
+
+
+
+            Random rand = new Random();
+            aJ[0] = Convert.ToUInt32(rand.Next(0, 100));
+            for (int k = 1; k < M+1; k++)
+            {
+                if (k == 0)
+                    aJ[k] = Convert.ToUInt32(rand.Next(Convert.ToInt32(aJ[0]), 100));
+                else
+                    aJ[k] = Convert.ToUInt32(rand.Next(Convert.ToInt32(aJ[k - 1]), 100));
+            
+            for (int i = 0; i < k; i++)
+            {
+
+                bJ[i] = (aJ[i + 1] + aJ[i]) / 2;
+               
+                double ss = standart_sapma(aJ);
+                double mu = ortalama(aJ);
+                for (int j = 0; j < k; j++)
+                {
+                    gauss[j] = gauss_dagilimi(aJ[j], mu, ss);
+
+                }
+           
+               
+            }
+                MSE_er = MSE(aJ, bJ);
+
+                label1.Text = "MSE=" + MSE_er;
+                mse_arr[mseindex] = MSE_er;
+                if (mseindex >= 1)
+                {
+                    if (MSE_er*100/100 <= mse_arr[mseindex - 1] )
+                        break;
+                }
+                mseindex++;
+            }
+
+            scottPlotUC1.Clear();
+            scottPlotUC1.PlotSignal(aJ, 1, Color.Blue);
+            scottPlotUC2.Clear();
+            scottPlotUC2.PlotSignal(gauss, 1, Color.Blue);
+            scottPlotUC3.Clear();
+            scottPlotUC3.PlotSignal(mse_arr, 1, Color.Blue);
+
+            if (needsAutoScaling)
+            {
+                scottPlotUC1.AxisAuto();
+                scottPlotUC2.AxisAuto();
+                scottPlotUC3.AxisAuto();
+                needsAutoScaling = false;
+            }
+
+            Application.DoEvents();
+
+
+        }
+
+        private void autoScaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            needsAutoScaling = true;
+        }
+
+        private void infoMessageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string msg = "";
+            msg += "left-click-drag to pan\n";
+            msg += "right-click-drag to zoom\n";
+            msg += "middle-click to auto-axis\n";
+            msg += "double-click for graphing stats\n";
+            MessageBox.Show(msg);
+        }
+
+
+
+        public double gauss_dagilimi(double x, double mu, double ss)
+        {
+
+            double çarpan;
+            çarpan = Convert.ToDouble(1 / Math.Sqrt(2 * Math.PI * ss * ss));
+            return çarpan * Math.Pow(Math.E, (-1 * (x - mu) * (x - mu) / (2 * ss * ss)));
+        }
+
+        public double MSE(double[] aj1, double[] bj1)
+        {
+
+            double sum_sq = 0;
+            double mse;
+
+            for (int i = 0; i < aj1.Length - 1; ++i)
+            {
+
+
+                double p1 = aj1[i];
+                double p2 = bj1[i];
+                double err = p2 - p1;
+                sum_sq += (err * err);
+
+            }
+            mse = (double)sum_sq / (bj1.Length);
+            return mse;
+        }
+        double ortalama(double[] liste)
+        {
+            int liste_sayisi = 0;
+            double liste_toplami = 0;
+            double ortalama = 0;
+
+            int i = 0;
+
+            while (liste_sayisi < liste.Length)
+            {
+                liste_toplami += liste[liste_sayisi];
+                liste_sayisi += 1;
+            }
+            ortalama = liste_toplami / liste_sayisi;
+            return ortalama;
+        }
+        double standart_sapma(double[] liste)
+        {
+            int liste_sayisi = 0;
+            double liste_toplami = 0;
+            double ortalama = 0;
+            double fark_karesi = 0;
+            double genel_toplam = 0;
+            int i = 0;
+
+            while (liste_sayisi < liste.Length)
+            {
+                liste_toplami += liste[liste_sayisi];
+                liste_sayisi += 1;
+            }
+            ortalama = liste_toplami / liste_sayisi;
+
+
+            while (i < liste_sayisi)
+            {
+                fark_karesi = liste[i] - ortalama;
+                fark_karesi = fark_karesi * fark_karesi;
+                fark_karesi = fark_karesi / (liste_sayisi - 1);
+                genel_toplam += fark_karesi;
+
+                i = i + 1;
+            }
+
+            genel_toplam = Math.Sqrt(genel_toplam);
+
+
+            return genel_toplam;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            PlotLatestData();
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+      
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Control)
+                this.AutoScroll = true;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Control)
+                this.HScroll = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            scottPlotUC1.AxisAuto();
+            scottPlotUC2.AxisAuto();
+            scottPlotUC3.AxisAuto();
+        }
+    }
+}
